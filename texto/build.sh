e43b7206b73a53052f72d6b6fd2dd5b6533893b2
Renan Birck Pinheiro <renan.ee.ufsm@gmail.com>
2013-06-06 22:32:54 -0300

    formatação; mais POG

--- texto/build.sh
+++ texto/build.sh
@@ -1,6 +1,11 @@
 FILENAME=apostila_LTspice
+# PDF_READER=evince
+# PDF_READER=okular
+PDF_READER=atril
 
 pdflatex $FILENAME.tex
 bibtex $FILENAME.aux
 pdflatex $FILENAME.tex
 pdflatex $FILENAME.tex
+
+$PDF_READER $FILENAME.pdf
